1218bdca9b415b9967738cad1115ada0d7888311
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-12-04 14:12:49 +0100

    fix(ui): update nest config

--- apps/ui/next.config.ts
+++ apps/ui/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  transpilePackages: [
+    "@reputo/algorithm-validator",
+    "@reputo/reputation-algorithms",
+  ],
 }
 
 export default nextConfig
