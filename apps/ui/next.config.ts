4412e464cf617dc12bacf190f12cb0011b5e4b5b
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-12-04 13:57:11 +0100

    build(ui): update nest config

--- apps/ui/next.config.ts
+++ apps/ui/next.config.ts
@@ -1,15 +1,7 @@
-import path from "node:path"
 import type { NextConfig } from "next"
-
-const workspaceRoot = path.join(__dirname, "../../")
 
 const nextConfig: NextConfig = {
   output: "standalone",
-  experimental: {
-    externalDir: true,
-  },
-  transpilePackages: ["@reputo/reputation-algorithms"],
-  outputFileTracingRoot: workspaceRoot,
 }
 
 export default nextConfig
