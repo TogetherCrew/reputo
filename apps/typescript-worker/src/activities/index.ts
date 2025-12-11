125035365b54b12c12c41d06b677cb4e422e243e
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-12-11 08:38:40 +0100

    build: fix build error

--- apps/typescript-worker/src/activities/index.ts
+++ apps/typescript-worker/src/activities/index.ts
@@ -3,5 +3,4 @@
  *
  * Each exported function should match an AlgorithmDefinition.runtime.activity value.
  */
-export * from './behzad.activity.js';
 export * from './voting_engagement.activity.js';
