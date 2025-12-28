17fc9fa6970f2c7ac87081b0c33d8ece19c93582
Behzad-rabiei <behzad.rabiei.77@gmail.com>
2025-12-28 12:57:05 +0100

    feat(reputation-algorithms): add proposal engagement and contribution score proposals

--- apps/typescript-worker/src/activities/index.ts
+++ apps/typescript-worker/src/activities/index.ts
@@ -3,4 +3,7 @@
  *
  * Each exported function should match an AlgorithmDefinition.runtime.activity value.
  */
+
+export * from './contribution_score.activity.js';
+export * from './proposal_engagement.activity.js';
 export * from './voting_engagement.activity.js';
